ebd25541b7b01b3332a4e428589628014f5f2f2f
董闽 <dongmin_isoft@xhqb.com>
2018-04-08 15:21:41 +0800

    test

---
+++ newMethods.js
@@ -0,0 +1,22 @@
+
+//数据处理
+initActivityListData = (shortVideoList) => {
+        
+    if (shortVideoList) {
+      const dataTemp = Object.assign([], shortVideoList)
+      var pageData = []
+      if (dataTemp.length > 0) {
+        const count = 2
+
+        for (let i = 0; i < dataTemp.length; i++) {
+          let page = Math.floor(i / count)
+          pageData[page] = pageData[page] || []
+          pageData[page].push(dataTemp[i])
+        }
+      }
+      this.showActInfoList = pageData
+    } else {
+      this.showActInfoList = []
+    }
+    
+}
